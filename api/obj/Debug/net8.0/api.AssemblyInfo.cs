374b1ae1cb25b8c43201c1a0018e077760218603
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-04-30 21:09:23 -0700

    made db file

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d28a01fee61cd758023cae58b920ff4a410560")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2487b140eb7c1d82faea0b32553158488783fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
