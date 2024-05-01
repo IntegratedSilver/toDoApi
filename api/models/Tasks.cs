da2487b140eb7c1d82faea0b32553158488783fd
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-04-30 21:06:53 -0700

    push 1

---
+++ api/models/Tasks.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.models;
+
+    public class Tasks
+{
+        public int Id { get; set; }
+        public string? objective1{ get; set; }
+        public string? Objective2{ get; set; }
+        public string? Objective3{ get; set; }
+        public string? Objective4{ get; set; }
+        public string? Objective5{ get; set; }
+}
